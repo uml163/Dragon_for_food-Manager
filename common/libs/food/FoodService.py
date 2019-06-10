186688f3790e2c9ea3122b2f77ed7e56e8408049
zfr0411 <37231438+zfr0411@users.noreply.github.com>
2019-06-10 10:04:00 +0800

    Update FoodService.py

--- common/libs/food/FoodService.py
+++ common/libs/food/FoodService.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from application import app,db
+from application import db
 from common.models.food.FoodStockChangeLog import FoodStockChangeLog
 from common.models.food.Food import Food
 from common.libs.Helper import getCurrentDate
@@ -15,13 +15,13 @@ class FoodService():
         if not food_info:
             return False
 
-        model_stock_change = FoodStockChangeLog()
-        model_stock_change.food_id = food_id
-        model_stock_change.unit = quantity
-        model_stock_change.total_stock = food_info.stock
-        model_stock_change.note = note
-        model_stock_change.created_time = getCurrentDate()
-        db.session.add(model_stock_change)
+        FoodStockChangeLog_model = FoodStockChangeLog()
+        FoodStockChangeLog_model.food_id = food_id
+        FoodStockChangeLog_model.unit = quantity
+        FoodStockChangeLog_model.total_stock = food_info.stock
+        FoodStockChangeLog_model.note = note
+        FoodStockChangeLog_model.created_time = getCurrentDate()
+        db.session.add(FoodStockChangeLog_model)
         db.session.commit()
         return True
 
