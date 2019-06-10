5fb49b26030e65e16e553878bea5c792083f2b42
zfr0411 <37231438+zfr0411@users.noreply.github.com>
2019-06-10 11:19:45 +0800

    Update CartService.py

--- common/libs/member/CartService.py
+++ common/libs/member/CartService.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-/Users/shuufuyou/Desktop/test/common/libs/member/CartService.py
-import hashlib,requests,random,string,json
-from application import app,db
+#购物车功能实现
+from application import db
 from common.models.member.MemberCart import MemberCart
 from common.libs.Helper import getCurrentDate
 class CartService():
@@ -16,20 +16,21 @@ class CartService():
 
     @staticmethod
     def setItems( member_id = 0,food_id = 0,number = 0 ):
-        if member_id < 1 or food_id < 1 or number < 1:
-            return False
+        #if member_id < 1 or food_id < 1 or number < 1:
+            #return False
         cart_info = MemberCart.query.filter_by( food_id = food_id, member_id= member_id ).first()
         if cart_info:
-            model_cart = cart_info
+            MemberCart_model = cart_info
         else:
-            model_cart = MemberCart()
-            model_cart.member_id = member_id
-            model_cart.created_time = getCurrentDate()
+            MemberCart_model = MemberCart()
+            MemberCart_model.member_id = member_id
+            MemberCart_model.created_time = getCurrentDate()
 
-        model_cart.food_id = food_id
-        model_cart.quantity = number
-        model_cart.updated_time = getCurrentDate()
-        db.session.add(model_cart)
+        MemberCart_model.food_id = food_id
+        MemberCart_model.quantity = number
+        MemberCart_model.updated_time = getCurrentDate()
+        db.session.add(MemberCart_model)
         db.session.commit()
         return True
+
 
