2491c9c9b4a9d3ad9b4b1533f7d72a1d14c9155d
zfr0411 <37231438+zfr0411@users.noreply.github.com>
2019-06-25 09:36:16 +0800

    数据库文件

---
+++ 后台初始账户.sql
@@ -0,0 +1,5 @@
+请将下面的SQL语句执行，然后默认登录账号是54php.cn 密码是123456
+
+INSERT INTO `user` (`uid`, `nickname`, `mobile`, `email`, `sex`, `avatar`, `login_name`, `login_pwd`, `login_salt`, `status`, `updated_time`, `created_time`)
+VALUES
+	(1, 'zfr163.top', '11012345679', '532536324@qq.com', 1, '', 'zfr163.cn', '816440c40b7a9d55ff9eb7b20760862c', 'cF3JfH5FJfQ8B2Ba', 1, '2019-03-15 14:08:48', '2019-03-15 14:08:48');
